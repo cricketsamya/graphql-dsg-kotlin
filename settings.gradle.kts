217f55824cfadfcbffec8714c13f47811a3ca11e
Sameer Kulkarni <s.kulkarni@tignum.com>
2022-01-24 14:16:57 +0100

    add: basic setup

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "graphql-dsg-kotlin"
---
+++ src/main/kotlin/com/graphql/dsg/GraphqlDsgKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.graphql.dsg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlDsgKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphqlDsgKotlinApplication>(*args)
+}
